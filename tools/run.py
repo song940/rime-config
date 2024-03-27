eacbb5e28b6910b5ef3c3602c0a1de964f26282a
Lsong <song940@gmail.com>
2024-03-27 20:43:09 +0800

    update

--- tools/run.py
+++
@@ -1,8 +0,0 @@
-import os,sys
-
-workDir = os.path.abspath(os.path.dirname(sys.path[0]))
-
-list=["update.py","clear.py","emoji2dict.py","util.py"]
-
-for file in list:
-    os.system(f"python {os.path.join(workDir,"tools",file)}")
